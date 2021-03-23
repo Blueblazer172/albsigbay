6d2fc818e58d51000db90877e3b36387568328a6
has <hs@hs-ai.eu>
2021-03-23 13:02:38 +0100

    Added sqlite tables, async books load on home page,

---
+++ db.js
@@ -0,0 +1,63 @@
+const sqlite3 = require('sqlite3').verbose();
+const DB_NAME = 'db.sqlite';
+
+let db = new sqlite3.Database(DB_NAME, (err) => {
+    if (err) {
+        // Cannot open database
+        console.error(err.message);
+        throw err;
+    } else {
+        console.log('Connected to the SQLite database.');
+        db.run(`
+            CREATE TABLE users
+            (
+                id           INTEGER PRIMARY KEY AUTOINCREMENT,
+                surname      text,
+                name         text,
+                email        text NOT NULL UNIQUE,
+                password     text,
+                city         text,
+                zip          INTEGER,
+                street       text,
+                streetNumber INTEGER,
+                rights       text,
+                CONSTRAINT email_unique UNIQUE (email)
+            )`, (err) => {
+            if (err) console.log('Already created users Table');
+        });
+
+        db.run(`
+            CREATE TABLE books
+            (
+                id              INTEGER PRIMARY KEY AUTOINCREMENT,
+                title           text,
+                isbn            INTEGER NOT NULL UNIQUE,
+                author          text,
+                pages           INTEGER,
+                description     text,
+                category        text,
+                vendor          text,
+                publicationDate text,
+                cover           text,
+                CONSTRAINT isbn_unique UNIQUE (isbn)
+            )`, (err) => {
+            if (err) console.log('Already created books Table');
+        });
+
+        db.run(`
+            CREATE TABLE borrowedBooks
+            (
+                id           INTEGER PRIMARY KEY AUTOINCREMENT,
+                bookId       INTEGER NOT NULL,
+                userId       INTEGER NOT NULL,
+                borrowedDate text,
+                returnDate   text,
+                FOREIGN KEY (bookId) REFERENCES books (bookId)
+                    FOREIGN KEY (userId) REFERENCES users(userId)
+            )`, (err) => {
+            if (err) console.log('Already created borrowedBooks Table');
+        });
+    }
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,42 +1,91 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bcrypt = require('bcrypt');
+const md5 = require('md5');
+const db = require('./db');
 const port = 3000;
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/books/covers'));
 
-app.get('/', function(req, res) {
+app.get("/api/users", (req, res, next) => {
+    let sql = "select * from users"
+    let params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+
+app.get("/api/books", (req, res, next) => {
+    let sql = "select * from books"
+    let params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+
+app.get("/api/borrowed-books", (req, res, next) => {
+    let sql = "select * from borrowedBooks"
+    let params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname + '/about.html'));
 });
 
-app.get('/faq', function(req, res) {
+app.get('/faq', function (req, res) {
     res.sendFile(path.join(__dirname + '/faq.html'));
 });
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
     res.sendFile(path.join(__dirname + '/login.html'));
 });
 
-app.get('/gdpr', function(req, res) {
+app.get('/gdpr', function (req, res) {
     res.sendFile(path.join(__dirname + '/gdpr.html'));
 });
 
-app.post('/login', function(req, res) {
+app.post('/login', function (req, res) {
     //@TODO handle post login
 });
 
-app.get('/register', function(req, res) {
+app.get('/register', function (req, res) {
     res.sendFile(path.join(__dirname + '/register.html'));
 });
 
-app.post('/register', function(req, res) {
+app.post('/register', function (req, res) {
     //@TODO handle post register
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`App listening at http://localhost:${port}`)
+});
