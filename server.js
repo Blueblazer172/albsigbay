b904a11740b5ed8feee804bf75b8ce0a296ab0ac
Oppani <oppanium@googlemail.com>
2021-03-23 14:08:13 +0100

    Navbar created, Css-File created, Css for Navbar

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.post('/register', function(req, res) {
     //@TODO handle post register
 });
 
+app.use(express.static(__dirname + '/plain-html-prototype'));
+
 app.get('/proto', function(req, res) {
     res.sendFile(path.join(__dirname + '/plain-html-prototype/index.html'));
 });
