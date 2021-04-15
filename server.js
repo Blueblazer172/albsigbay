ad7c801f1ac0540d0dfa5d44c9f7d899f194b5c4
has <hs@hs-ai.eu>
2021-04-15 11:02:05 +0200

    Updated file paths, Added and updated routes, fixed path imports

--- server.js
+++ server.js
@@ -3,44 +3,62 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/assets'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/about', function(req, res) {
-    res.sendFile(path.join(__dirname + '/about.html'));
+    res.sendFile(path.join(__dirname + '/views/pages/about.html'));
 });
 
 app.get('/faq', function(req, res) {
-    res.sendFile(path.join(__dirname + '/faq.html'));
+    res.sendFile(path.join(__dirname + '/views/pages/faq.html'));
 });
 
+app.get('/datenschutz', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/pages/datenschutz.html'));
+});
+
+app.get('/impressum', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/pages/impressum.html'));
+});
+
+app.get('/book', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/components/book.html'));
+});
+
+app.get('/edit-book', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/components/edit-book.html'));
+});
+
+app.get('/profile', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/components/profile.html'));
+});
+
+app.get('/verwaltung', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/components/verwaltung.html'));
+});
+
+
+// Handle login
 app.get('/login', function(req, res) {
-    res.sendFile(path.join(__dirname + '/login.html'));
-});
-
-app.get('/gdpr', function(req, res) {
-    res.sendFile(path.join(__dirname + '/gdpr.html'));
+    res.sendFile(path.join(__dirname + '/views/pages/login.html'));
 });
 
 app.post('/login', function(req, res) {
     //@TODO handle post login
 });
 
+// Handle register
 app.get('/register', function(req, res) {
-    res.sendFile(path.join(__dirname + '/register.html'));
+    res.sendFile(path.join(__dirname + '/views/pages/register.html'));
 });
 
 app.post('/register', function(req, res) {
     //@TODO handle post register
-});
-
-app.use(express.static(__dirname + '/plain-html-prototype'));
-
-app.get('/proto', function(req, res) {
-    res.sendFile(path.join(__dirname + '/plain-html-prototype/index.html'));
 });
 
 app.listen(port, () => {
