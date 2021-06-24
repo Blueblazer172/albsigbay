9caac4bc2ce9827add976ddd2682ab9897f903c5
Oppani <Oppanium@googlemail.com>
2021-06-24 16:31:38 +0200

    changed testname in book.test.js

--- tests/book.test.js
+++ tests/book.test.js
@@ -11,12 +11,11 @@ test('check book 1 exists', () => {
 });
 
 
-test('has user 3 with the name test2 borrowed a book with id 1', () => {
+test('check if a user with firstname has borrowed book with id 1', () => {
     // user firstname with userId 1 is max
     return book.fetchBook(1).then(data => {
         return user.fetchUser(data.data.data.borrowedBooks[0].userId).then(data => {
             expect(data.data.data.firstname).toBe('test2')
         });
-
     });
 });
