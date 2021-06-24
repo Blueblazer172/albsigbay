5f2575c6a2396ef72227689ec51a052f1c333688
Oppani <Oppanium@googlemail.com>
2021-06-24 16:25:04 +0200

    added book tests, added user tests

---
+++ tests/book.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+
+const book = {
+    fetchBook: (bookId) => {
+        return axios.post('http://localhost:4000/api/book',{bookId: bookId});
+    }
+}
+
+
+
+module.exports = book;
